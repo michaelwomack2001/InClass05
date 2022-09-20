7e541fcbcdc10a406e650678c760ae6235afa3aa
michaelwomack2001 <michaelwomack2001@gmail.com>
2022-09-19 22:15:10 -0400

    App list view

--- app/src/main/java/edu/uncc/inclass05/fragments/AppsListFragment.java
+++ app/src/main/java/edu/uncc/inclass05/fragments/AppsListFragment.java
@@ -9,9 +9,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import edu.uncc.inclass05.R;
+import edu.uncc.inclass05.databinding.FragmentAppCategoriesBinding;
 import edu.uncc.inclass05.databinding.FragmentAppsListBinding;
+import edu.uncc.inclass05.models.DataServices;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +27,10 @@ import edu.uncc.inclass05.databinding.FragmentAppsListBinding;
 public class AppsListFragment extends Fragment {
 
     FragmentAppsListBinding binding;
+    ArrayList<DataServices.App> appCategoryList = new ArrayList<DataServices.App>();
+    ArrayAdapter<DataServices.App> adapter;
+    ListView listView;
+    String Tag;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -65,9 +75,29 @@ public class AppsListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        binding = FragmentAppsListBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        View appListView = inflater.inflate(R.layout.fragment_apps_list, container,false);
+        if(Tag.equals("New Apps We Love")){
+            appCategoryList = DataServices.getAppsByCategory(Tag);
+        }
+        else if(Tag.equals("New Games We Love")){
+            appCategoryList = DataServices.getAppsByCategory(Tag);
+        }
+        else if(Tag.equals("Top Free Apps")){
+            appCategoryList = DataServices.getAppsByCategory(Tag);
+        }
+        else if(Tag.equals(("Top Grossing Apps"))){
+            appCategoryList = DataServices.getAppsByCategory(Tag);
+        }
+        else if(Tag.equals("Top Paid Apps")){
+            appCategoryList = DataServices.getAppsByCategory(Tag);
+        }
+
+        listView = appListView.findViewById(R.id.appList);
+        adapter = new ArrayAdapter<DataServices.App>(getContext(), android.R.layout.simple_expandable_list_item_1, appCategoryList);
+        listView.setAdapter(adapter);
+        return appListView;
     }
 
     @Override
